78acd6a0a1f92659cb0fa2571526738ab73ec92d
Rakesh <rake7h@gmail.com>
2024-02-11 22:39:43 +0530

    Update publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-set -e
+set -e -x
 
 # Get the current branch name
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
+echo $CURRENT_BRANCH
+
 # Set the npm dist-tag based on the branch name
-if [ "$CURRENT_BRANCH" != "main" ]; then
-  DIST_TAG="experimental"
-else
+if [ "$CURRENT_BRANCH" == "main" ]; then
   DIST_TAG="latest"
+else
+  DIST_TAG="experimental"
 fi
 
+echo $DIST_TAG
+
 # Publish the package with the appropriate dist-tag
-npm publish --tag "$DIST_TAG"
+# npm publish --tag "$DIST_TAG"
